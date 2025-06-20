5fd7bb6815922733d449152b9c57b81cc2ec424b
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2025-06-20 08:30:29 -0400

    Update service (CRUD) & import category entity to category module (necessary to categoryRepository to work in the service)

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,26 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FindManyOptions, Repository } from 'typeorm';
+import { Category } from './entities/category.entity';
 
 @Injectable()
 export class CategoriesService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+
   create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+    // const category = new Category()
+    // category.name=createCategoryDto.name
+    // return this.categoryRepository.save(category);
+    return this.categoryRepository.save(createCategoryDto);
   }
 
   findAll() {
-    return `This action returns all categories`;
+    return this.categoryRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async findOne(id: number) {
+    const options: FindManyOptions<Category> = {
+      where: {
+        id,
+      },
+    };
+
+    const category = await this.categoryRepository.findOne(options);
+    if (!category) {
+      // throw new HttpException('La Categoría no existe',404); // esta es generica y hay que colocar manualmente el status_code
+      throw new NotFoundException('La Categoría no existe'); // esta es especifica y ya viene con status_code 404, asi como esta esta hay otras exceptions
+    }
+    return category;
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+  async update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    const category = await this.findOne(id);
+    category.name = updateCategoryDto.name;
+    return await this.categoryRepository.save(category);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async remove(id: number) {
+    const category = await this.findOne(id);
+    await this.categoryRepository.remove(category);
+    return 'Categoría Eliminada';
   }
 }
