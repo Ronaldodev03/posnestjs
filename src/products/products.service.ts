1a2da33566d37f1346ac4e33654a9ee3ec450e0c
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2025-06-23 16:01:45 -0400

    Update product service (create) & import entities needed in the module

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from 'src/categories/entities/category.entity';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    const category = await this.categoryRepository.findOneBy({
+      id: createProductDto.categoryId,
+    });
+
+    if (!category) {
+      let errors: string[] = [];
+      errors.push('La Categoría no existe'); // coloco los errores en un array para que sea consistente con los errores del Dto
+      throw new NotFoundException(errors);
+    }
+
+    return this.productRepository.save({
+      ...createProductDto,
+      category, // en la entity de category hay un cascade en true (en la relacion OneToMany), con eso podemos pasar el objeto de catogory completo aqui y tendremos habilitada la info de la category en el product
+    });
   }
 
   findAll() {
