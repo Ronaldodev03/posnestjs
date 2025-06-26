a0df3efdd322dd8f6f1dde9887f244e2987a4741
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2025-06-26 03:21:44 -0400

    Update transaction dto

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,38 @@
-export class CreateTransactionDto {}
+import { Type } from 'class-transformer';
+import {
+  ArrayNotEmpty,
+  IsArray,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  Length,
+  ValidateNested,
+} from 'class-validator';
+
+export class TransactionContentsDto {
+  @IsNotEmpty({ message: 'El ID del producto no puede estar vacío' })
+  @IsInt({ message: 'Producto no válido' })
+  productId: number;
+
+  @IsNotEmpty({ message: 'Cantidad no puede estar vacía' })
+  @IsInt({ message: 'Cantidad no válida' }) // Validate quantity too
+  quantity: number;
+
+  @IsNotEmpty({ message: 'Precio no puede estar vacío' })
+  @IsNumber({}, { message: 'Precio no válido' })
+  price: number;
+}
+
+export class CreateTransactionDto {
+  @IsNotEmpty({ message: 'El Total no puede ir vacio' })
+  @IsNumber({}, { message: 'Cantidad no válida' })
+  total: number;
+
+  // aqui validos nested objets que cumplan con el Dto de TransactionContentsDto
+  @IsArray()
+  @ArrayNotEmpty({ message: 'Los Contenidos no pueden ir vacios' })
+  @ValidateNested()
+  @Type(() => TransactionContentsDto)
+  contents: TransactionContentsDto[];
+}
